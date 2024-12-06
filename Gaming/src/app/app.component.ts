e679904c3f12446a5b054271913200b5bbe1cc6d
Nikolai Dimitrov <nikolaidimitrov9012@gmail.com>
2024-12-06 23:02:10 +0200

    Add Search Page

--- Gaming/src/app/app.component.ts
+++ Gaming/src/app/app.component.ts
@@ -3,10 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
+import { SearchComponent } from '../search/search.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, HeaderComponent, FooterComponent],
+  imports: [HomeComponent, HeaderComponent, FooterComponent, RegisterComponent, LoginComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
